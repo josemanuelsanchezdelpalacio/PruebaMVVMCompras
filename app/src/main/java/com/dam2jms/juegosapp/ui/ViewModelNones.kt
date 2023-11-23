e5a1471cc52d797d3b3be0ae9047299e4b97ee88
josemanuelsanchezdelpalacio <170068@iessanalberto.com>
2023-11-23 10:14:59 +0100

    juego 7 y medio con viewmodel

--- app/src/main/java/com/dam2jms/juegosapp/ui/ViewModelNones.kt
+++ app/src/main/java/com/dam2jms/juegosapp/ui/ViewModelNones.kt
@@ -10,9 +10,9 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import kotlin.random.Random
 
-class ViewModelNones:ViewModel() {
+class ViewModelNones : ViewModel() {
 
-    private val _puntuacion = MutableLiveData<Int>()
+    private val _puntuacion = MutableLiveData<Int>(0)
     val puntuacion: LiveData<Int> = _puntuacion
 
     private val _seleccionJugador = MutableLiveData<String>()
@@ -20,9 +20,6 @@ class ViewModelNones:ViewModel() {
 
     private val _numeroJugador = MutableLiveData<Int>()
     val numeroJugador: LiveData<Int> = _numeroJugador
-
-    private val _numeroPC = MutableLiveData<Int>()
-    val numeroPC: LiveData<Int> = _numeroPC
 
     private val _resultado = MutableLiveData<String>()
     val resultado: LiveData<String> = _resultado
@@ -38,44 +35,33 @@ class ViewModelNones:ViewModel() {
         val seleccionPC: String = if (_seleccionJugador.value == "nones") "pares" else "nones"
 
         // genero un numero aleatorio entre 1 y 5 para la PC
-        _numeroPC.value = (1..5).random()
-        val suma = _numeroJugador.value!! + _numeroPC.value!!
+        val numeroPC = (1..5).random()
+        val suma = _numeroJugador.value!! + numeroPC!!
 
         //compruebo que la seleccion del jugador solo pueda ser pares o nones
-        when (_seleccionJugador.value?.toLowerCase()) {
-            "pares", "nones" -> {
+        if (_seleccionJugador.value != "pares" || _seleccionJugador.value != "nones") {
 
-                // si el numero del jugador es mayor o igual a 1 y es menor o igual a 5.
-                // si no, saca mensaje de que el numero tiene que ser entre 1 y 5
-                if (_numeroJugador.value in 1..5) {
-                    // si es nones y es impar la suma o si es pares y es par la suma
-                    if ((_seleccionJugador.value == "nones" && suma % 2 != 0) || (_seleccionJugador.value == "pares" && suma % 2 == 0)) {
-                        _resultado.value = "ganas"
-                        _puntuacion.value = (_puntuacion.value ?: 0) + 1
-                    } else {
-                        _resultado.value = "pierdes"
-                        val nuevaPuntuacion = (_puntuacion.value ?: 0) - 1
-                        //para que no pueda bajar la puntuacion de 0
-                        _puntuacion.value = if (nuevaPuntuacion < 0) 0 else nuevaPuntuacion
-                    }
+            // compruebo que el numero del jugador sea entre 1 y 5
+            if (_numeroJugador.value in 1..5) {
 
-                    //muestro el resultado
-                    _resultado.value =
-                        "Jugador: ${_seleccionJugador.value} ${_numeroJugador.value}\n PC: $seleccionPC. ${_numeroPC.value}\n Resultado: ${_resultado.value}"
-
+                // si es nones y es impar la suma o si es pares y es par la suma
+                if ((_seleccionJugador.value == "nones" && suma % 2 != 0) || (_seleccionJugador.value == "pares" && suma % 2 == 0)) {
+                    _resultado.value = "ganas"
+                    _puntuacion.postValue(puntuacion.value?.plus(10))
                 } else {
-                    Toast.makeText(context, "El numero debe ser entre 1 y 5", Toast.LENGTH_SHORT)
-                        .show()
-                }
-            }
+                    _resultado.value = "pierdes"
+                    _puntuacion.postValue(puntuacion.value?.minus(5))
 
-            else -> {
-                Toast.makeText(
-                    context,
-                    "Solo puedes elejir entre pares o nones",
-                    Toast.LENGTH_SHORT
-                ).show()
+                }
+
+                //muestro el resultado
+                _resultado.value = "Jugador: ${_seleccionJugador.value} ${_numeroJugador.value}\n PC: $seleccionPC. ${numeroPC}\n Resultado: ${_resultado.value}"
+
+            } else {
+                Toast.makeText(context, "El numero debe ser entre 1 y 5", Toast.LENGTH_SHORT).show()
             }
+        } else {
+            Toast.makeText(context, "Solo puedes elejir entre pares o nones", Toast.LENGTH_SHORT).show()
         }
     }
 }
