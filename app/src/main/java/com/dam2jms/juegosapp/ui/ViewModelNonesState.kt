86ca1a5415bef898ee598df5e8147b71eafa768e
josemanuelsanchezdelpalacio <92978100+josemanuelsanchezdelpalacio@users.noreply.github.com>
2023-11-27 15:47:37 +0100

    Update ViewModelNonesState.kt

--- app/src/main/java/com/dam2jms/juegosapp/ui/ViewModelNonesState.kt
+++ app/src/main/java/com/dam2jms/juegosapp/ui/ViewModelNonesState.kt
@@ -6,58 +6,46 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class ViewModelNonesState : ViewModel() {
+class NonesViewModel : ViewModel() {
+    // Propiedades del estado del juego
+    private val _uiState = MutableStateFlow(GameUiState())
+    val uiState: StateFlow<GameUiState> = _uiState
 
-    private val _puntuacion = MutableLiveData<Int>(0)
-    val puntuacion: LiveData<Int> = _puntuacion
-
-    private val _seleccionJugador = MutableLiveData<String>()
-    val seleccionJugador: LiveData<String> = _seleccionJugador
-
-    private val _numeroJugador = MutableLiveData<Int>()
-    val numeroJugador: LiveData<Int> = _numeroJugador
-
-    private val _resultado = MutableLiveData<String>()
-    val resultado: LiveData<String> = _resultado
-
-
-    fun onSeleccionJugador(seleccionJugadorUi: String, numeroJugadorUi: Int) {
-        _seleccionJugador.value = seleccionJugadorUi
-        _numeroJugador.value = numeroJugadorUi
+    // Método para manejar cambios en la selección y el número del jugador
+    fun onChange(seleccion: String, numero: Int) {
+        _uiState.value = _uiState.value.copy(seleccionJugador = seleccion, numeroJugador = numero)
     }
-
+    
+    // Método para iniciar el juego
     fun onJuego(context: Context) {
-        // genero de forma aleatoria que el PC escoja entre pares o nones
-        val seleccionPC: String = if (_seleccionJugador.value == "nones") "pares" else "nones"
-
-        // genero un numero aleatorio entre 1 y 5 para la PC
+        val seleccionPC: String = if (_uiState.value.seleccionJugador == "nones") "pares" else "nones"
         val numeroPC = (1..5).random()
-        val suma = _numeroJugador.value!! + numeroPC!!
+        val suma = _uiState.value.numeroJugador + numeroPC
 
-        //compruebo que la seleccion del jugador solo pueda ser pares o nones
-        if (_seleccionJugador.value != "pares" || _seleccionJugador.value != "nones") {
-
-            // compruebo que el numero del jugador sea entre 1 y 5
-            if (_numeroJugador.value in 1..5) {
-
-                // si es nones y es impar la suma o si es pares y es par la suma
-                if ((_seleccionJugador.value == "nones" && suma % 2 != 0) || (_seleccionJugador.value == "pares" && suma % 2 == 0)) {
-                    _resultado.value = "ganas"
-                    _puntuacion.postValue(puntuacion.value?.plus(10))
+        if (_uiState.value.seleccionJugador == "pares" || _uiState.value.seleccionJugador == "nones") {
+            if (_uiState.value.numeroJugador in 1..5) {
+                val resultadoJuego = if ((_uiState.value.seleccionJugador == "nones" && suma % 2 != 0) || (_uiState.value.seleccionJugador == "pares" && suma % 2 == 0)) {
+                    _uiState.value = _uiState.value.copy(
+                        puntuacion = _uiState.value.puntuacion + 10,
+                        resultado = "ganas"
+                    )
+                    "ganas"
                 } else {
-                    _resultado.value = "pierdes"
-                    _puntuacion.postValue(puntuacion.value?.minus(5))
-
+                    _uiState.value = _uiState.value.copy(
+                        puntuacion = _uiState.value.puntuacion - 5,
+                        resultado = "pierdes"
+                    )
+                    "pierdes"
                 }
 
-                //muestro el resultado
-                _resultado.value = "Jugador: ${_seleccionJugador.value} ${_numeroJugador.value}\n PC: $seleccionPC. ${numeroPC}\n Resultado: ${_resultado.value}"
-
+                _uiState.value = _uiState.value.copy(
+                    resultado = "Jugador: ${_uiState.value.seleccionJugador} ${_uiState.value.numeroJugador}\nPC: $seleccionPC. $numeroPC\nResultado: $resultadoJuego"
+                )
             } else {
-                Toast.makeText(context, "El numero debe ser entre 1 y 5", Toast.LENGTH_SHORT).show()
+                Toast.makeText(context, "El número debe estar entre 1 y 5", Toast.LENGTH_SHORT).show()
             }
         } else {
-            Toast.makeText(context, "Solo puedes elejir entre pares o nones", Toast.LENGTH_SHORT).show()
+            Toast.makeText(context, "Solo puedes elegir entre pares o nones", Toast.LENGTH_SHORT).show()
         }
     }
 }
