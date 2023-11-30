1b0fea456dabb2b026b764003c5aa860638184b2
josemanuelsanchezdelpalacio <92978100+josemanuelsanchezdelpalacio@users.noreply.github.com>
2023-11-30 17:22:07 +0100

    Delete PruebaNavigation directory

--- PruebaNavigation/app/src/main/java/com/dam2jms/pruebanavigation/navigation/AppScreens.kt
+++
@@ -1,6 +0,0 @@
-package com.dam2jms.pruebanavigation.navigation
-
-sealed class AppScreens (val route: String){
-    object FirstScreen: AppScreens(route = "firs_screen")
-    object SecondScreen: AppScreens(route = "second_screen")
-}
